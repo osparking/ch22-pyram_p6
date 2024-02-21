bd9233e64638f99b2a70f12603fbffbec8d0f603
osparking <jbpark03@gmail.com>
2024-02-21 15:11:01 +0900

    오이 시험의 진입점인 빈 클래스

--- src/test/java/space/bum/junit/jb_p6/airport/CucumberTest.java
+++ src/test/java/space/bum/junit/jb_p6/airport/CucumberTest.java
@@ -1,5 +1,19 @@
 package space.bum.junit.jb_p6.airport;
 
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+/**
+ * JUnit 으로 오이 시험을 실행할 때 진입점.
+ */
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = { "pretty" }, features = "classpath:features")
 public class CucumberTest {
 
+  /**
+   * 이 클래스는 빈 클래스인 것이 정상이다. 
+   * 단계 정의는 다른 클래스(예, PassengersPolicy)에 들어있어야 된다.
+   */
 }
