9dfd037b6245351ac762f08e94c0feed2f794a2b
osparking <jbpark03@gmail.com>
2024-02-22 11:17:48 +0900

    FlightNumber 애노테이션

---
+++ src/test/java/space/bum/junit/jb_p6/airport/annot/FlightNumber.java
@@ -0,0 +1,17 @@
+package space.bum.junit.jb_p6.airport.annot;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import javax.inject.Qualifier;
+
+@Qualifier
+@Retention(RUNTIME)
+@Target({ FIELD, METHOD })
+public @interface FlightNumber {
+  String number();
+}
